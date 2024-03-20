203ee5114a470e4c246c2db6b5974930810501b3
Liang Wang <wangliang1124@163.com>
2024-03-20 10:56:30 +0800

    enhance getWikipediaImage, deal with timeout

--- src/apis/wikipediaImageApi.js
+++ src/apis/wikipediaImageApi.js
@@ -1,27 +1,26 @@
 export async function getWikipediaImage(city) {
   let image;
   if (city) {
-    let response = await fetch(
-      `https://en.wikipedia.org/w/api.php?action=opensearch&search=${city}&format=json&redirects=resolve&limit=1`
-    );
+    let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${city}&format=json&redirects=resolve&limit=1`
+    let response = await timeoutPromise(fetch(url), 500)
 
     if (response && response.ok) {
       let data = await response.json();
 
       if (data && data.length === 4 && data[3].length >= 1) {
         let page = data[3][0].replace('https://en.wikipedia.org/wiki/', '');
+        let imageUrl = `https://en.wikipedia.org/w/api.php?action=query&titles=${page}&prop=pageimages&format=json&pithumbsize=1000`
+        let imageResponse = await timeoutPromise(fetch(imageUrl), 3000);
 
-        let imageResponse = await fetch(
-          `https://en.wikipedia.org/w/api.php?action=query&titles=${page}&prop=pageimages&format=json&pithumbsize=1000`
-        );
-
-        let imageData = await imageResponse.json();
-        if (imageData && imageData.query && imageData.query.pages) {
-          for (const pageNum in imageData.query.pages) {
-            let pageData = imageData.query.pages[pageNum];
-            if (pageData && pageData.thumbnail && pageData.thumbnail.source) {
-              if (!pageData.thumbnail.source.includes('svg')) {
-                image = pageData.thumbnail.source;
+        if (imageResponse && imageResponse.ok) {
+          let imageData = await imageResponse.json();
+          if (imageData && imageData.query && imageData.query.pages) {
+            for (const pageNum in imageData.query.pages) {
+              let pageData = imageData.query.pages[pageNum];
+              if (pageData && pageData.thumbnail && pageData.thumbnail.source) {
+                if (!pageData.thumbnail.source.includes('svg')) {
+                  image = pageData.thumbnail.source;
+                }
               }
             }
           }
@@ -32,3 +31,11 @@ export async function getWikipediaImage(city) {
 
   return image;
 }
+
+const timeoutPromise = (promise, ms = 5000) => {
+  let timeout = new Promise(function (resolve, reject) {
+    setTimeout(resolve, ms, null);
+  });
+
+  return Promise.race([promise, timeout]);
+};
